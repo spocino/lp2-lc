54f07f1ac3d8b63425d15d596f44808c87b2cba4
Samuel Pocino <sam.pocino@gmail.com>
2023-11-07 15:08:13 -0500

    Init project

---
+++ Lp2lc.lean
@@ -0,0 +1,3 @@
+-- This module serves as the root of the `Lp2lc` library.
+-- Import modules here that should be built as part of the library.
+import «Lp2lc».Basic
---
+++ Main.lean
@@ -0,0 +1,4 @@
+import «Lp2lc»
+
+def main : IO Unit :=
+  IO.println s!"Hello, {hello}!"
